dc48fb330c3bb2bd66a3680f357a4068267f9d68
Sven Bellamy <sven.bellamy@gmail.com>
2021-03-30 04:18:52 +0000

    adding two numbers from command line

---
+++ w1/d1-focal/sum.js
@@ -0,0 +1,19 @@
+const args = process.argv;
+const sliced = args.slice(2);
+const sum = Number(sliced[0]) + Number(sliced[1]);
+console.log(sum);
+
+
+/*
+const sliced = args.slice(2, 4);
+
+const a = sliced[0].Math;
+const b = sliced[1].Math;
+const sum = Math.a + Math.b;
+
+console.log(sum);
+*/
+
+
+// const sliced = args.slice[1];
+

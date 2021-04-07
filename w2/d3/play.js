940923d1292c35f1fe2846db0815c2c84deb88c3
Sven Bellamy <sven.bellamy@gmail.com>
2021-04-07 23:06:47 +0000

    snek connect edit for client setup

---
+++ w2/d3/play.js
@@ -0,0 +1,27 @@
+// snek-multiplayer
+
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: '10.0.0.117',
+    port: 50541
+  });
+  //interpret incoming data as text
+  conn.setEncoding('utf8');
+
+  conn.on('data', (data) => {
+    console.log(data);
+    
+  })
+
+  return conn;
+};
+
+
+console.log('Connecting ...');
+connect();

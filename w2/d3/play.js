31fc9bfb25add4b8183d2b3319a716e6b0331b04
Sven Bellamy <sven.bellamy@gmail.com>
2021-04-08 00:44:11 +0000

    client.js added and code copied from play.js

--- w2/d3/play.js
+++ w2/d3/play.js
@@ -1,27 +1,6 @@
 // snek-multiplayer
 
-const net = require('net');
-
-/**
- * Establishes connection with the game server
- */
-
-const connect = function() {
-  const conn = net.createConnection({
-    host: '10.0.0.117',
-    port: 50541
-  });
-  //interpret incoming data as text
-  conn.setEncoding('utf8');
-
-  conn.on('data', (data) => {
-    console.log(data);
-    
-  })
-
-  return conn;
-};
-
+const { connect } = require('./client');
 
 console.log('Connecting ...');
 connect();
